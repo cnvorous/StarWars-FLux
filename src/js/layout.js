9ebe1aecb142e6f7835ecff660ef6014697c8c3c
cnvorous <cnvorous@gmail.com>
2022-02-15 02:12:45 +0000

    added Bootstrap cards and fetch

--- src/js/layout.js
+++ src/js/layout.js
@@ -22,18 +22,10 @@ const Layout = () => {
 				<ScrollToTop>
 					<Navbar />
 					<Switch>
-						<Route exact path="/">
-							<Home />
-						</Route>
-						<Route exact path="/demo">
-							<Demo />
-						</Route>
-						<Route exact path="/single/:theid">
-							<Single />
-						</Route>
-						<Route>
-							<h1>Not found!</h1>
-						</Route>
+						<Route exact path="/" component={Home}/>
+						<Route exact path="/demo" component={Demo}/>
+						<Route exact path="/single/:theid" component={Single}/>
+						<Route render={()=> <h1>Not found!</h1>}/>
 					</Switch>
 					<Footer />
 				</ScrollToTop>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,47 @@
-import React from "react";
+import React, { useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import {StarWarsCard} from "../component/StarWarsCard"; // ../component cause getting out of views folder 
+// two large collections of items means two arrays 
+//create reusable card component 
+//map through arrays and render card for each
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
+//home is landing page 
+
+export const Home = () => {
+
+	const [personCard, setPersonCard] =useState([]);
+	const [planetCard, setPlanetCard]= useState([]);
+
+	useEffect(() => {
+		fetch(https://swapi.dev/api/people/, {
+			method: "GET", 
+		}) 
+		.then((response) => {
+			return response.json();
+		})
+		.then((characters) => {
+			setPersonCard(characters.results);
+		});
+	}, 
+	[]);
+
+	useEffect(() => {
+		fetch(https://swapi.dev/api/planets/?page=2", {
+			method: "GET", 
+		}) 
+		.then((response) => {
+			return response.json();
+		})
+		.then((planets) => {
+			setPlanetCard(planents.results);
+		});
+	}, 
+	[]);
+
+	return (
+	<div>
+		<StarWarsCard/>
 	</div>
 );
+}
